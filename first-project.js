79bd62decfd078e524b1ffac60ceaaa50fa2b66f
danila-beep <danil123w29@gmail.com>
2023-02-06 19:03:25 +0300

    first commit

---
+++ first-project.js
@@ -0,0 +1,14 @@
+//простые математические действия и оператор console.log
+const a = 10 + 10;
+    console.log(a);
+const b = 10 - 10;
+    console.log(b);
+const c = 10 / 10;
+    console.log(c);
+const d = 10 * 10;
+    console.log(d);
+let e = 12.1*13.4;
+    console.log(e)
+//простые математические действия и оператор console.log
+
+
